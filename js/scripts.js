6fb96837b3d8e45324dd72468752c6aa929a59a3
Alexandra Duran <duranalexandra2004@gmail.com>
2021-10-05 10:00:26 -0500

    fist commit

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+const btn_a = document.getElementById("btn-a")
+const audio_a = document.getElementById("audio-b")
+const btn_b = document.getElementById("btn-b")
+const audio_b = document.getElementById("audio-b")
+const btn_c = document.getElementById("btn-c")
+const audio_c = document.getElementById("audio-c")
+const btn_d = document.getElementById("btn-d")
+const audio_d = document.getElementById("audio-d")
+const btn_e = document.getElementById("btn-e")
+const audio_e = document.getElementById("audio-e")
+const btn_f = document.getElementById("btn-f")
+const audio_f = document.getElementById("audio-f")
+const btn_g = document.getElementById("btn-g")
+const audio_g = document.getElementById("audio-g")
+const btn_h = document.getElementById("btn-h")
+const audio_h = document.getElementById("audio-h")
+const btn_i = document.getElementById("btn-i")
+const audio_i = document.getElementById("audio-i")
+
+btn_a.addEventListener('click', ()=>{
+    audio_a.play();
+})
+
+btn_b.addEventListener('click', ()=>{
+    audio_b.play();
+})
+
+btn_c.addEventListener('click', ()=>{
+    audio_c.play();
+})
+
+btn_d.addEventListener('click', ()=>{
+    audio_d.play();
+})
+
+btn_e.addEventListener('click', ()=>{
+    audio_e.play();
+})
+
+btn_f.addEventListener('click', ()=>{
+    audio_f.play();
+})
+
+btn_g.addEventListener('click', ()=>{
+    audio_g.play();
+})
+
+btn_h.addEventListener('click', ()=>{
+    audio_h.play();
+})
+
+btn_i.addEventListener('click', ()=>{
+    audio_i.play();
+})
